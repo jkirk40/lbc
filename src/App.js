9b243ab0811bd2bfa395a40b873e37d49a7fad9f
Jamie <kirkwood.jamie@gmail.com>
2021-03-05 17:14:53 -0500

    first fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {useEffect} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('https://www.lbcit.ca/demo/api/?key=d9658b9d-4f86-491f-bd67-86af0c547a5c')
+    .then((res) => {
+      console.log(res)
+      return res.json()
+    })
+    .then((data) => {
+      console.log(data)
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
